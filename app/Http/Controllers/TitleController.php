b0e873170edbcc7c91bbcc2f52ab0bdc01906551
vinh.nguyenthanh <vinh.nguyenthanh@amela.vn>
2023-07-24 16:43:28 +0700

    initial

---
+++ app/Http/Controllers/TitleController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\TitleModel;
+use Exception;
+use Illuminate\Support\Facades\DB;
+
+class TitleController extends Controller
+{
+    public function index()
+    {
+        $titles = TitleModel::all();
+
+        return $titles;
+    }
+
+    public function store(Request $request)
+    {
+        DB::beginTransaction();
+        try {
+            $data = $request->all();
+            $title = TitleModel::create($data);
+            DB::commit();
+
+            return $title;
+        } catch(Exception $e) {
+            DB::rollBack();
+
+            return $e;
+        }
+    }
+
+    public function edit(Request $request, $id)
+    {
+        DB::beginTransaction();
+        try {
+            $title = TitleModel::findOrFail($id);
+            $data = $request->all();
+            $title->update($data);
+            DB::commit();
+
+            return $title;
+        } catch(Exception $e) {
+            DB::rollBack();
+
+            return $e;
+        }
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        DB::beginTransaction();
+        try {
+            $title = TitleModel::findOrFail($id);
+            $title->delete();
+            DB::commit();
+
+            return [
+                'message' => 'success'
+            ];
+        } catch(Exception $e) {
+            DB::rollBack();
+
+            return $e;
+        }
+    }
+}
