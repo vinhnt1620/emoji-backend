d2a1f2a359a414fe978ee6ac07128ac88af84894
vinh.nguyenthanh <vinh.nguyenthanh@amela.vn>
2023-08-09 16:08:36 +0700

    test

--- app/Http/Controllers/TitleController.php
+++ app/Http/Controllers/TitleController.php
@@ -14,7 +14,7 @@ class TitleController extends Controller
     {
         $titles = TitleModel::first();
 
-        // logger()->error('ERROR', ['titles' => $titles]);
+        logger()->error('DEMO-ERROR', ['titles' => $titles]);
 
         return $titles;
     }
